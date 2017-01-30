5f26a7d8da2542af09110ae1d0340fcda333433c
René Hermenau <admin@xsimulator.net>
2017-01-30 16:07:49 +0100

    randomize feedback form

--- includes/admin/views/deactivate-feedback.php
+++ includes/admin/views/deactivate-feedback.php
@@ -1,18 +1,41 @@
+<?php 
+$reasons = array(
+    		1 => '<li><label><input type="radio" name="quads_disable_reason" value="temporary"/>' . __('It is only temporary', 'quick-adsense-reloaded') . '</label></li>',
+		2 => '<li><label><input type="radio" name="quads_disable_reason" value="stopped showing ads"/>' . __('I stopped showing ads on my site', 'quick-adsense-reloaded') . '</label></li>',
+		3 => '<li><label><input type="radio" name="quads_disable_reason" value="missing feature"/>' . __('I miss a feature', 'quick-adsense-reloaded') . '</label></li>
+		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Please describe the feature"/></li>',
+		4 => '<li><label><input type="radio" name="quads_disable_reason" value="technical issue"/>' . __('Technical Issue', 'quick-adsense-reloaded') . '</label></li>
+		<li><textarea name="quads_disable_text[]" placeholder="' . __('Can we help? Please describe your problem', 'quick-adsense-reloaded') . '"></textarea></li>',
+		5 => '<li><label><input type="radio" name="quads_disable_reason" value="other plugin"/>' . __('I switched to another plugin', 'quick-adsense-reloaded') .  '</label></li>
+		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Name of the plugin"/></li>',
+		6 => '<li><label><input type="radio" name="quads_disable_reason" value="other"/>' . __('Other reason', 'quick-adsense-reloaded') . '</label></li>
+		<li><textarea name="quads_disable_text[]" placeholder="' . __('Please specify, if possible', 'quick-adsense-reloaded') . '"></textarea></li>',
+    );
+shuffle($reasons);
+?>
+
+
 <div id="quick-adsense-reloaded-feedback-overlay" style="display: none;">
     <div id="quick-adsense-reloaded-feedback-content">
 	<form action="" method="post">
 	    <h3><strong><?php _e('If you have a moment, please let us know why you are deactivating:', 'quick-adsense-reloaded'); ?></strong></h3>
 	    <ul>
-		<li><label><input type="radio" name="quads_disable_reason" value="temporary"/><?php _e('It is only temporary', 'quick-adsense-reloaded'); ?></label></li>
+<!--		<li><label><input type="radio" name="quads_disable_reason" value="temporary"/><?php _e('It is only temporary', 'quick-adsense-reloaded'); ?></label></li>
 		<li><label><input type="radio" name="quads_disable_reason" value="stopped showing ads"/><?php _e('I stopped showing ads on my site', 'quick-adsense-reloaded'); ?></label></li>
 		<li><label><input type="radio" name="quads_disable_reason" value="missing feature"/><?php _e('I miss a feature', 'quick-adsense-reloaded'); ?></label></li>
 		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Which one?"/></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="technical issue"/><?php _e('I have a technical issue', 'quick-adsense-reloaded'); ?></label></li>
-		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Can we help? Please let us know how', 'quick-adsense-reloaded'); ?>"></textarea></li>
+		<li><label><input type="radio" name="quads_disable_reason" value="technical issue"/><?php _e('Technical Issue', 'quick-adsense-reloaded'); ?></label></li>
+		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Can we help? Please let us know your issue', 'quick-adsense-reloaded'); ?>"></textarea></li>
 		<li><label><input type="radio" name="quads_disable_reason" value="other plugin"/><?php _e('I switched to another plugin', 'quick-adsense-reloaded'); ?></label></li>
 		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Which one?"/></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="other"/><?php _e('other reason', 'quick-adsense-reloaded'); ?></label></li>
-		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Please specify, if possible', 'quick-adsense-reloaded'); ?>"></textarea></li>
+		<li><label><input type="radio" name="quads_disable_reason" value="other"/><?php _e('Other reason', 'quick-adsense-reloaded'); ?></label></li>
+		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Please specify, if possible', 'quick-adsense-reloaded'); ?>"></textarea></li>-->
+                <?php 
+                //wp_die(print_r($quads_feedback));
+                foreach ($reasons as $reason){
+                    echo $reason;
+                }
+                ?>
 	    </ul>
 	    <?php if ($email) : ?>
     	    <input type="hidden" name="quads_disable_from" value="<?php echo $email; ?>"/>
