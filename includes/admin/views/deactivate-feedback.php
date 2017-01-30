fb0de35151724e032ae0261ca5984e02136d531e
René Hermenau <admin@xsimulator.net>
2017-01-30 16:10:01 +0100

    randomize feedback form

--- includes/admin/views/deactivate-feedback.php
+++ includes/admin/views/deactivate-feedback.php
@@ -20,18 +20,7 @@ shuffle($reasons);
 	<form action="" method="post">
 	    <h3><strong><?php _e('If you have a moment, please let us know why you are deactivating:', 'quick-adsense-reloaded'); ?></strong></h3>
 	    <ul>
-<!--		<li><label><input type="radio" name="quads_disable_reason" value="temporary"/><?php _e('It is only temporary', 'quick-adsense-reloaded'); ?></label></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="stopped showing ads"/><?php _e('I stopped showing ads on my site', 'quick-adsense-reloaded'); ?></label></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="missing feature"/><?php _e('I miss a feature', 'quick-adsense-reloaded'); ?></label></li>
-		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Which one?"/></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="technical issue"/><?php _e('Technical Issue', 'quick-adsense-reloaded'); ?></label></li>
-		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Can we help? Please let us know your issue', 'quick-adsense-reloaded'); ?>"></textarea></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="other plugin"/><?php _e('I switched to another plugin', 'quick-adsense-reloaded'); ?></label></li>
-		<li><input type="text" name="quads_disable_text[]" value="" placeholder="Which one?"/></li>
-		<li><label><input type="radio" name="quads_disable_reason" value="other"/><?php _e('Other reason', 'quick-adsense-reloaded'); ?></label></li>
-		<li><textarea name="quads_disable_text[]" placeholder="<?php _e('Please specify, if possible', 'quick-adsense-reloaded'); ?>"></textarea></li>-->
                 <?php 
-                //wp_die(print_r($quads_feedback));
                 foreach ($reasons as $reason){
                     echo $reason;
                 }
