89c0cccb4055e486cd4d133fda10935a872057f4
Lokesh Jasthi <jasthilokesh@gmail.com>
2020-03-09 23:41:09 +0530

    #62 author,plugin uri changes for mu-plugin

--- includes/mu-plugin/wpquads_remove_shortcode.php
+++ includes/mu-plugin/wpquads_remove_shortcode.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Plugin Name: WP QUADS
- * Plugin URI: https://wordpress.org/plugins/quick-adsense-reloaded/
+ * Plugin Name: WP QUADS Shortcode Remover
+ * Plugin URI: https://wpquads.com/
  * Description: Remove WP QUADS shortcode when the plugin AdSense Integration WP QUADS has deactive/uninstall
  * Author: WP Quads
- * Author URI: https://wordpress.org/plugins/quick-adsense-reloaded/
+ * Author URI: https://wpquads.com/
  * Version: 0.1
- * Credits: WP QUADS - Quick AdSense Reloaded is a fork of Quick AdSense
  */
 // Exit if accessed directly
 if( !defined( 'ABSPATH' ) )
@@ -17,7 +16,7 @@ if( !defined( 'WP_QUADS_MU_PLUGIN_VERSION' ) ) {
 }
 
 if(!defined( 'QUADS_NAME' ) && !class_exists( 'QuickAdsenseReloaded' )){
-	add_shortcode( 'quads_ad', 'quads_shortcode_remove_ad', 1); 
+	add_shortcode( 'quads_ad', 'wpquads_remove_unsed_shortcode', 1); 
 	add_shortcode( 'quads', 'wpquads_remove_unsed_shortcode', 1);
 	function wpquads_remove_unsed_shortcode( $atts ) {
 		return '';
