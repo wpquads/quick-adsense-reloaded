b466b248e0b75dd567398fc171640a907c0a7438
Lokesh Jasthi <jasthilokesh@gmail.com>
2020-04-30 14:38:09 +0530

    #96 Fixed importer issue

--- admin/includes/setup.php
+++ admin/includes/setup.php
@@ -54,6 +54,69 @@ class QUADS_Ad_Setup {
     
                         if($key === 'ad'.$i){
                             
+                            if(isset($quads_settings['pos1'])){ 
+                            if($quads_settings['pos1']['BegnAds'] == 1){
+                                if(isset($quads_settings['pos1']['BegnRnd']) && $quads_settings['pos1']['BegnRnd']== $i){ 
+                                    $value['position']                      = 'beginning_of_post';  
+                                }
+                            }
+                        } 
+
+                        if(isset($quads_settings['pos2'])){ 
+                            if($quads_settings['pos2']['MiddAds'] == 1){
+                                if(isset($quads_settings['pos2']['MiddRnd']) && $quads_settings['pos2']['MiddRnd']== $i){ 
+                                    $value['position']                      = 'middle_of_post';  
+                                }
+                            }
+                        }
+                         if(isset($quads_settings['pos3'])){ 
+                            if($quads_settings['pos3']['EndiRnd'] == 1){
+                                if(isset($quads_settings['pos3']['EndiRnd']) && $quads_settings['pos3']['EndiRnd']== $i){ 
+                                    $value['position']                      = 'end_of_post';  
+                                }
+                            }
+                        }
+                         if(isset($quads_settings['pos4'])){ 
+                            if($quads_settings['pos4']['MoreRnd'] == 1){
+                                if(isset($quads_settings['pos4']['MoreRnd']) && $quads_settings['pos4']['MoreRnd']== $i){ 
+                                    $value['position']                      = 'after_more_tag';  
+                                }
+                            }
+                        }
+                         if(isset($quads_settings['pos5'])){ 
+                            if($quads_settings['pos5']['LapaRnd'] == 1){
+                                if(isset($quads_settings['pos5']['LapaRnd']) && $quads_settings['pos5']['LapaRnd']== $i){ 
+                                    $value['position']                      = 'middle_of_post';  
+                                }
+                            }
+                        }
+                         if(isset($quads_settings['pos6'])){ 
+                            if($quads_settings['pos6']['Par1Ads'] == 1){
+                                if(isset($quads_settings['pos6']['Par1Rnd']) && $quads_settings['pos6']['Par1Rnd']== $i){ 
+                                    $value['position']                      = 'after_paragraph';  
+                                    $value['paragraph_number']              = $quads_settings['pos6']['Par1Nup'];
+                                    $value['enable_on_end_of_post']              = $quads_settings['pos6']['Par1Con'];
+                                }
+                            }
+                        }
+                        if(isset($quads_settings['pos7'])){ 
+                            if($quads_settings['pos7']['Par2Ads'] == 1){
+                                if(isset($quads_settings['pos7']['Par2Rnd']) && $quads_settings['pos7']['Par2Rnd']== $i){ 
+                                    $value['position']                      = 'after_paragraph';  
+                                    $value['paragraph_number']              = $quads_settings['pos7']['Par2Nup'];
+                                    $value['enable_on_end_of_post']              = $quads_settings['pos7']['Par2Con'];
+                                }
+                            }
+                        }
+                        if(isset($quads_settings['pos8'])){ 
+                            if($quads_settings['pos8']['Par3Rnd'] == 1){
+                                if(isset($quads_settings['pos8']['Par3Rnd']) && $quads_settings['pos8']['Par3Rnd']== $i){ 
+                                    $value['position']                      = 'after_paragraph';  
+                                    $value['paragraph_number']              = $quads_settings['pos8']['Par3Nup'];
+                                    $value['enable_on_end_of_post']              = $quads_settings['pos8']['Par3Con'];
+                                }
+                            }
+                        }
                             $post_id = quadsGetPostIdByMetaKeyValue('quads_ad_old_id', $key); 
                             
                             if($post_id){                            
