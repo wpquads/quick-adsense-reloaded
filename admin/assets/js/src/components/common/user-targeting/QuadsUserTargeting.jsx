0daaaea92119ead60702947479bcd970e617a021
mali <merajali294@gmail.com>
2022-04-01 19:12:41 +0530

    #415
    Added support to render Ad based on language selection

--- admin/assets/js/src/components/common/user-targeting/QuadsUserTargeting.jsx
+++ admin/assets/js/src/components/common/user-targeting/QuadsUserTargeting.jsx
@@ -151,6 +151,80 @@ class QuadsUserTargeting extends Component {
                     { value:'ipad', label: 'iPad'},
                     { value:'ipod', label: 'iPod'},
                 ],
+                multilingual_language:[
+                    { value:'af', label: 'Afrikanns'}  ,
+                    { value:'sq', label: 'Albanian'}  ,
+                    { value:'ar', label: 'Arabic'}  ,
+                    { value:'hy', label: 'Armenian'}  ,
+                    { value:'eu', label: 'Basque'}  ,
+                    { value:'bn', label: 'Bengali'}  ,
+                    { value:'bg', label: 'Bulgarian'}  ,
+                    { value:'ca', label: 'Catalan'}  ,
+                    { value:'km', label: 'Cambodian'}  ,
+                    { value:'zh', label: 'Chinese (Mandarin)'}  ,
+                    { value:'hr', label: 'Croation'}  ,
+                    { value:'cs', label: 'Czech'}  ,
+                    { value:'da', label: 'Danish'}  ,
+                    { value:'nl', label: 'Dutch'}  ,
+                    { value:'en', label: 'English'}  ,
+                    { value:'et', label: 'Estonian'}  ,
+                    { value:'fj', label: 'Fiji'}  ,
+                    { value:'fi', label: 'Finnish'}  ,
+                    { value:'fr', label: 'French'}  ,
+                    { value:'ka', label: 'Georgian'}  ,
+                    { value:'de', label: 'German'}  ,
+                    { value:'el', label: 'Greek'}  ,
+                    { value:'gu', label: 'Gujarati'}  ,
+                    { value:'he', label: 'Hebrew'}  ,
+                    { value:'hi', label: 'Hindi'}  ,
+                    { value:'hu', label: 'Hungarian'}  ,
+                    { value:'is', label: 'Icelandic'}  ,
+                    { value:'id', label: 'Indonesian'}  ,
+                    { value:'ga', label: 'Irish'}  ,
+                    { value:'it', label: 'Italian'}  ,
+                    { value:'ja', label: 'Japanese'}  ,
+                    { value:'jw', label: 'Javanese'}  ,
+                    { value:'ko', label: 'Korean'}  ,
+                    { value:'la', label: 'Latin'}  ,
+                    { value:'lv', label: 'Latvian'}  ,
+                    { value:'lt', label: 'Lithuanian'}  ,
+                    { value:'mk', label: 'Macedonian'}  ,
+                    { value:'ms', label: 'Malay'}  ,
+                    { value:'ml', label: 'Malayalam'}  ,
+                    { value:'mt', label: 'Maltese'}  ,
+                    { value:'mi', label: 'Maori'}  ,
+                    { value:'mr', label: 'Marathi'}  ,
+                    { value:'mn', label: 'Mongolian'}  ,
+                    { value:'ne', label: 'Nepali'}  ,
+                    { value:'no', label: 'Norwegian'}  ,
+                    { value:'fa', label: 'Persian'}  ,
+                    { value:'pl', label: 'Polish'}  ,
+                    { value:'pt', label: 'Portuguese'}  ,
+                    { value:'pa', label: 'Punjabi'}  ,
+                    { value:'qu', label: 'Quechua'}  ,
+                    { value:'ro', label: 'Romanian'}  ,
+                    { value:'ru', label: 'Russian'}  ,
+                    { value:'sm', label: 'Samoan'}  ,
+                    { value:'sr', label: 'Serbian'}  ,
+                    { value:'sk', label: 'Slovak'}  ,
+                    { value:'sl', label: 'Slovenian'}  ,
+                    { value:'es', label: 'Spanish'}  ,
+                    { value:'sw', label: 'Swahili'}  ,
+                    { value:'sv', label: 'Swedish '}  ,
+                    { value:'ta', label: 'Tamil'}  ,
+                    { value:'tt', label: 'Tatar'}  ,
+                    { value:'te', label: 'Telugu'}  ,
+                    { value:'th', label: 'Thai'}  ,
+                    { value:'bo', label: 'Tibetan'}  ,
+                    { value:'to', label: 'Tonga'}  ,
+                    { value:'tr', label: 'Turkish'}  ,
+                    { value:'uk', label: 'Ukranian'}  ,
+                    { value:'ur', label: 'Urdu'}  ,
+                    { value:'uz', label: 'Uzbek'}  ,
+                    { value:'vi', label: 'Vietnamese'}  ,
+                    { value:'cy', label: 'Welsh'},
+                    { value:'xh', label: 'Xhosa'}
+                ],
                 user_type:[
                     { value:'administrator', label: 'Administrator'},
                     { value:'editor', label: 'Editor'},
@@ -445,6 +519,19 @@ class QuadsUserTargeting extends Component {
             this.setState({excludedToggle: !this.state.excludedToggle});
         }
     }
+
+    componentDidMount(){
+        const wpml_activation = quads_localize_data.wpml_activation
+        const multiTypeOptions = this.state.multiTypeOptions
+
+        if( wpml_activation == 1 ){
+            multiTypeOptions.push( { label:'Language', value:'multilingual_language' } )
+            this.setState({
+                multiTypeOptions:multiTypeOptions
+            })
+        }
+    }
+
     componentDidUpdate (){
         const include = this.state.multiTypeIncludedValue;
         const exclude = this.state.multiTypeExcludedValue;
