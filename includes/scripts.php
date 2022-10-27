b9da8a9b375ea95773f6237d6309f21d28a0e520
Akshay Wali <akshaywali@live.in>
2022-10-27 18:56:56 +0530

    #625 Lightbox alignment issues: https://take.ms/6u6Iy

--- includes/scripts.php
+++ includes/scripts.php
@@ -392,7 +392,8 @@ function quads_inline_styles() {
     .quads-popupad {
         position: fixed;
         top: 0px;
-        width: 68em;
+        left:0px;
+        width: 100%;
         height: 100em;
         background-color: rgba(0,0,0,0.6);
         z-index: 999;
