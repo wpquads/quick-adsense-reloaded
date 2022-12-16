4a6005b0ec94a6e7068a2143f012971ec37e673d
Pradip <103096459+pradipshende@users.noreply.github.com>
2022-12-16 19:15:28 +0530

    All Ads should appear after 3-4 seconds of loading the page. #646
    issue in admin panel

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -3,10 +3,12 @@ var wpquads_adblocker_check = true;
 var wpquads_adblocker_check_2 = true;
 
 window.addEventListener("load", function(){
-    var wpquads_sticky = document.querySelector(".quads-sticky .quads-location");
-    wpquads_sticky.style.backgroundColor = 'hsla(0,0%,100%,.7)';
-    var wpquads_location_ad = document.querySelectorAll(".quads-location");
     setTimeout(()=>{
+        var wpquads_sticky = document.querySelector(".quads-sticky .quads-location");
+        var wpquads_location_ad = document.querySelectorAll(".quads-location");
+        if(wpquads_sticky){
+            wpquads_sticky.style.backgroundColor = 'hsla(0,0%,100%,.7)';
+        }
         Array.from(wpquads_location_ad).forEach(elm=>{
             elm.style.display = "block";
         })
