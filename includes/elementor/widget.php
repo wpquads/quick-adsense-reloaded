bd8d211ff36e0992fe3cd15422a076e4f4508db2
ankit644 <ankeetsingh65@gmail.com>
2023-01-20 12:31:44 +0530

    #703

--- includes/elementor/widget.php
+++ includes/elementor/widget.php
@@ -16,18 +16,18 @@ class Plugin {
 		require_once( __DIR__ . '/quads_elementor.php' );
 	}
 
-	public function register_widgets() {
+	public function register() {
 		// Its is now safe to include Widgets files
 		$this->include_widgets_files();
 
 		// Register Widgets
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Quads_Elementor() );
+		\Elementor\Plugin::instance()->widgets_manager->register( new Widgets\Quads_Elementor() );
 	}
 
 
 	public function __construct() {
 		// Register widgets
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
+		add_action( 'elementor/widgets/registered', [ $this, 'register' ] );
 	}
 }
 
