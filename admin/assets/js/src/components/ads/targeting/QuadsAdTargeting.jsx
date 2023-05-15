5e9005bcf800bf0f8cfe538c46d84740bbb52377
Rkroy <43204388+Rkroy@users.noreply.github.com>
2023-05-15 17:07:29 +0530

    #751
    Checkbox selection should work on every respective label click

--- admin/assets/js/src/components/ads/targeting/QuadsAdTargeting.jsx
+++ admin/assets/js/src/components/ads/targeting/QuadsAdTargeting.jsx
@@ -282,7 +282,7 @@ class QuadsAdTargeting extends Component {
                    { post_meta.position == 'ad_sticky_ad' ? 
                     <>
                   <tr>
-                  <td> <label htmlFor="sticy_ad_closebtn"> {__('Add close Button? ', 'quick-adsense-reloaded')}{post_meta.add_close_btn}</label> </td>
+                  <td> <label htmlFor="cls_btn"> {__('Add close Button? ', 'quick-adsense-reloaded')}{post_meta.add_close_btn}</label> </td>
                     <td><input id='cls_btn' checked={post_meta.cls_btn} name="cls_btn" onChange={this.props.adFormChangeHandler} type="checkbox"/></td>
                   </tr>
 
